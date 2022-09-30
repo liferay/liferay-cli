6ea23fdf7df642841773dff4996192d27a90f395
Raymond Augé <raymond.auge@liferay.com>
2022-09-30 08:25:20 -0400

    utility for working with Liferay .product_info.json

--- constants/constants.go
+++ constants/constants.go
@@ -3,6 +3,9 @@ package constants
 var Const = struct {
 	DockerLocaldevServerImage string
 	DockerNetwork             string
+	ReleasesEtag              string
+	ReleasesFile              string
+	ReleasesURL               string
 	RepoDir                   string
 	RepoRemote                string
 	RepoBranch                string
@@ -10,6 +13,9 @@ var Const = struct {
 }{
 	DockerLocaldevServerImage: "docker.localdev.server.image",
 	DockerNetwork:             "docker.network",
+	ReleasesEtag:              "releases.etag",
+	ReleasesFile:              "releases.file",
+	ReleasesURL:               "releases.url",
 	RepoDir:                   "repo.dir",
 	RepoRemote:                "repo.remote",
 	RepoBranch:                "repo.branch",
---
+++ releases/releases.go
@@ -0,0 +1,76 @@
+package releases
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+	"liferay.com/lcectl/constants"
+	"liferay.com/lcectl/http"
+)
+
+type Releases struct {
+}
+
+func init() {
+	dirname, err := os.UserHomeDir()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	viper.SetDefault(constants.Const.ReleasesFile, filepath.ToSlash(path.Join(dirname, ".lcectl", "releases.json")))
+	viper.SetDefault(constants.Const.ReleasesURL, "https://releases-cdn.liferay.com/tools/workspace/.product_info.json")
+}
+
+func ReleaseVersions(verbose bool) ([]string, error) {
+	dat, err := ReleasesJSON(verbose)
+
+	if err != nil {
+		return nil, err
+	}
+
+	keys := make([]string, len(dat))
+
+	i := 0
+	for k := range dat {
+		keys[i] = k
+		i++
+	}
+
+	return keys, nil
+}
+
+func GetReleaseByVersion(version string, verbose bool) (interface{}, error) {
+	dat, err := ReleasesJSON(verbose)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dat[version], nil
+}
+
+func ReleasesJSON(verbose bool) (map[string]interface{}, error) {
+	var dat map[string]interface{}
+
+	bytes, err := http.GetOrFetchBytes(http.GetOrFetchBytesOptions{
+		EtagKey: constants.Const.ReleasesEtag,
+		FileKey: constants.Const.ReleasesFile,
+		URL:     viper.GetString(constants.Const.ReleasesURL),
+		Verbose: verbose,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(bytes, &dat); err != nil {
+		return nil, err
+	}
+
+	return dat, nil
+}
