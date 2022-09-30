d381be0d5f51fac05a7530c8ddad099bd93b0eaa
Raymond Augé <raymond.auge@liferay.com>
2022-09-30 08:51:27 -0400

    add project name input to create wizard

--- cmd/ext/create.go
+++ cmd/ext/create.go
@@ -5,13 +5,17 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package ext
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
 
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 	"liferay.com/lcectl/cetypes"
 	"liferay.com/lcectl/prereq"
 )
+
+var whitespace = regexp.MustCompile(`\s`)
 
 // refreshCmd represents the refresh command
 var createCmd = &cobra.Command{
@@ -20,26 +24,49 @@ var createCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		prereq.Prereq(Verbose)
 
+		validate := func(input string) error {
+			if len(input) <= 0 {
+				return errors.New("Project Name must not be empty")
+			}
+			if whitespace.MatchString(input) {
+				return errors.New("Project Name must not contain spaces")
+			}
+			return nil
+		}
+
+		projectNamePrompt := promptui.Prompt{
+			Label:    "Project Name",
+			Validate: validate,
+		}
+
+		projectName, err := projectNamePrompt.Run()
+
+		if err != nil {
+			fmt.Printf("Project Name entry failed %v\n", err)
+			return
+		}
+
 		dat, err := cetypes.ClientExtensionTypeKeys(Verbose)
 
 		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
+			fmt.Printf("Error getting Client Extension Types %v\n", err)
 			return
 		}
 
-		prompt := promptui.Select{
-			Label: "Select Type",
+		cetPrompt := promptui.Select{
+			Label: "Type",
 			Items: dat,
 		}
 
-		_, result, err := prompt.Run()
+		_, cetType, err := cetPrompt.Run()
 
 		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
+			fmt.Printf("Error getting Client Extension Types %v\n", err)
 			return
 		}
 
-		fmt.Printf("You choose %q\n", result)
+		fmt.Printf("Project Name: %q\n", projectName)
+		fmt.Printf("Type: %q\n", cetType)
 	},
 }
 
