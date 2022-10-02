ec01591c91cff6ae452e9ba7a12688c04898e066
Raymond Augé <raymond.auge@liferay.com>
2022-10-02 12:25:55 -0400

    util to detect empty directory

---
+++ io/emptydir.go
@@ -0,0 +1,20 @@
+package io
+
+import (
+	"io"
+	"os"
+)
+
+func IsDirEmpty(name string) bool {
+	f, err := os.Open(name)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+
+	_, err = f.Readdirnames(1) // Or f.Readdir(1)
+	if err == io.EOF {
+		return true
+	}
+	return false
+}
