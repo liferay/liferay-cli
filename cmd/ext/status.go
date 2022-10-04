fb836c9c1f529f18ff13a4ac6824d7ff111c9ca3
Raymond Augé <raymond.auge@liferay.com>
2022-10-04 16:35:59 -0400

    ext status command

---
+++ cmd/ext/status.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package ext
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"liferay.com/lcectl/constants"
+	"liferay.com/lcectl/docker"
+	"liferay.com/lcectl/flags"
+	"liferay.com/lcectl/prereq"
+	"liferay.com/lcectl/spinner"
+)
+
+// refreshCmd represents the refresh command
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Returns the status of the extension environment",
+	Run: func(cmd *cobra.Command, args []string) {
+		prereq.Prereq(flags.Verbose)
+
+		config := container.Config{
+			Image: "localdev-server",
+			Cmd:   []string{"/repo/scripts/ext/status.sh"},
+		}
+		host := container.HostConfig{
+			Binds: []string{
+				fmt.Sprintf("%s:%s", viper.GetString(constants.Const.RepoDir), "/repo"),
+				docker.GetDockerSocket() + ":/var/run/docker.sock",
+				fmt.Sprintf("%s:/workspace/client-extensions", flags.ClientExtensionDir),
+				"localdevGradleCache:/root/.gradle",
+				"localdevLiferayCache:/root/.liferay",
+			},
+			NetworkMode: container.NetworkMode(viper.GetString(constants.Const.DockerNetwork)),
+		}
+
+		spinner.Spin(
+			spinner.SpinOptions{
+				Doing: "Status", Done: "is running", On: "'localdev' extension environment", Enable: flags.Verbose,
+			},
+			func(fior func(io.ReadCloser, bool)) int {
+				return docker.InvokeCommandInLocaldev("localdev-status", config, host, true, flags.Verbose, fior)
+			})
+	},
+}
+
+func init() {
+	extCmd.AddCommand(statusCmd)
+}
