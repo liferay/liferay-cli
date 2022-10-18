5224a4f2d23a694246497b7ed6c1352c25074087
Gregory Amerson <gregory.amerson@liferay.com>
2022-10-17 21:28:06 -0500

    copy files to localdev images

--- cmd/runtime/runtime.go
+++ cmd/runtime/runtime.go
@@ -5,9 +5,15 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package runtime
 
 import (
+	"log"
 	"os"
+	"path"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"liferay.com/lcectl/constants"
+	lio "liferay.com/lcectl/io"
+	"liferay.com/lcectl/mkcert"
 )
 
 // runtimeCmd represents the runtime command
@@ -19,6 +25,28 @@ var runtimeCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 		os.Exit(0)
+	},
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if cmd.Name() != "mkcert" {
+			lfrdevCrtFile := path.Join(viper.GetString(constants.Const.RepoDir), "/k8s/tls/lfrdev.crt")
+			lfrdevKeyFile := path.Join(viper.GetString(constants.Const.RepoDir), "/k8s/tls/lfrdev.key")
+			lfrdevRootCAFile1 := path.Join(viper.GetString(constants.Const.RepoDir), "/k8s/tls/", mkcert.GetRootName())
+			lfrdevRootCAFile2 := path.Join(viper.GetString(constants.Const.RepoDir), "/k8s/tls/", mkcert.GetRootName())
+			lfrdevRootCAFile3 := path.Join(viper.GetString(constants.Const.RepoDir), "/k8s/tls/", mkcert.GetRootName())
+
+			if !lio.Exists(lfrdevCrtFile) || !lio.Exists(lfrdevKeyFile) || !lio.Exists(lfrdevRootCAFile1) || !lio.Exists(lfrdevRootCAFile2) || !lio.Exists(lfrdevRootCAFile3) {
+				log.Fatalf("Missing one or more local certificates.  Execute 'runtime mkcert' command to generate one.")
+			}
+
+			crt, key, err := mkcert.LoadX509KeyPair(lfrdevCrtFile, lfrdevKeyFile)
+			if crt == nil || key == nil || err != nil {
+				log.Fatalf("Could not load x509 key pair: %s", err)
+			}
+			lrdevDomain := viper.GetString(constants.Const.TlsLfrdevDomain)
+			if crt.DNSNames[0] != "*."+lrdevDomain {
+				log.Fatalf("Generated certificate DNSName does not match configured domain: %s != %s\nPlease run 'runtime mkcert' command again.", crt.DNSNames[0], lrdevDomain)
+			}
+		}
 	},
 }
 
