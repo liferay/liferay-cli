91c9dd9c9869ac6435710f29a51a69ea7ebbf690
Gregory Amerson <gregory.amerson@liferay.com>
2022-10-20 21:28:50 -0500

    add build command

---
+++ cmd/ext/build.go
@@ -0,0 +1,59 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package ext
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"liferay.com/lcectl/constants"
+	"liferay.com/lcectl/docker"
+	"liferay.com/lcectl/flags"
+	"liferay.com/lcectl/spinner"
+)
+
+var buildCmd = &cobra.Command{
+	Use:   "build [override default build cmd]",
+	Short: "Executes the client extension workspace build to generate deployable artifacts (zip files)",
+	Args:  cobra.ArbitraryArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmdArgs := append([]string{"/repo/scripts/ext/build.sh"}, args...)
+
+		config := container.Config{
+			Image: "localdev-server",
+			Cmd:   cmdArgs,
+			Env: []string{
+				"LOCALDEV_REPO=/repo",
+				"LFRDEV_DOMAIN=" + viper.GetString(constants.Const.TlsLfrdevDomain),
+			},
+		}
+		host := container.HostConfig{
+			Binds: []string{
+				fmt.Sprintf("%s:%s", viper.GetString(constants.Const.RepoDir), "/repo"),
+				docker.GetDockerSocket() + ":/var/run/docker.sock",
+				fmt.Sprintf("%s:/workspace/client-extensions", flags.ClientExtensionDir),
+				"localdevGradleCache:/root/.gradle",
+				"localdevLiferayCache:/root/.liferay",
+				"localdevNodeModulesCache:/workspace/node_modules_cache",
+			},
+			NetworkMode: container.NetworkMode(viper.GetString(constants.Const.DockerNetwork)),
+		}
+
+		spinner.Spin(
+			spinner.SpinOptions{
+				Doing: "Build", Done: "is running", On: "'localdev' extension environment", Enable: !flags.Verbose,
+			},
+			func(fior func(io.ReadCloser, bool, string) int) int {
+				return docker.InvokeCommandInLocaldev("localdev-build", config, host, true, flags.Verbose, fior, "")
+			})
+	},
+}
+
+func init() {
+	extCmd.AddCommand(buildCmd)
+}
