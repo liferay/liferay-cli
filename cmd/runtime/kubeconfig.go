2fce74a034b18fdf4a2beb6690218464efc7395f
Gregory Amerson <gregory.amerson@liferay.com>
2022-10-24 13:38:44 -0500

    add `liferay runtime kubeconfig` command to write out .kube/config to communicate to cluster

---
+++ cmd/runtime/kubeconfig.go
@@ -0,0 +1,62 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package runtime
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"liferay.com/liferay/cli/constants"
+	"liferay.com/liferay/cli/docker"
+	"liferay.com/liferay/cli/flags"
+	"liferay.com/liferay/cli/spinner"
+)
+
+// kubeconfigCmd represents the kubeconfig command
+var kubeconfigCmd = &cobra.Command{
+	Use:   "kubeconfig",
+	Short: "Generates the kubeconfig necessary to talk to k8s cluster context",
+	Run: func(cmd *cobra.Command, args []string) {
+		userHomeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		config := container.Config{
+			Image: "localdev-server",
+			Cmd:   []string{"/repo/scripts/runtime/kubeconfig.sh"},
+			Env: []string{
+				"LOCALDEV_REPO=/repo",
+				"KUBECONFIG=/var/run/.kube/config",
+			},
+		}
+		host := container.HostConfig{
+			Binds: []string{
+				fmt.Sprintf("%s:%s", viper.GetString(constants.Const.RepoDir), "/repo"),
+				docker.GetDockerSocket() + ":/var/run/docker.sock",
+				filepath.Join(userHomeDir, ".kube") + ":/var/run/.kube",
+			},
+			NetworkMode: container.NetworkMode(viper.GetString(constants.Const.DockerNetwork)),
+		}
+
+		spinner.Spin(
+			spinner.SpinOptions{
+				Doing: "Writing", Done: "was written", On: "'kubeconfig' config file", Enable: !flags.Verbose,
+			},
+			func(fior func(io.ReadCloser, bool, string) int) int {
+				return docker.InvokeCommandInLocaldev("localdev-kubeconfig", config, host, true, flags.Verbose, fior, "")
+			})
+	},
+}
+
+func init() {
+	runtimeCmd.AddCommand(kubeconfigCmd)
+}
