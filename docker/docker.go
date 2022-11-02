9fae82149301d0047d3b6d0881f5f2fac73c79a7
Simon.Jiang <simon.jiang@liferay.com>
2022-11-02 14:12:13 +0800

    set DOCKER_HOST for windows

--- docker/docker.go
+++ docker/docker.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -117,7 +117,9 @@ func GetDockerClient() (*client.Client, error) {
 		return dockerClient, nil
 	}
 
-	if _, exists := os.LookupEnv("DOCKER_HOST"); !exists {
+	if runtime.GOOS == "windows" {
+		os.Setenv("DOCKER_HOST", "npipe:////.//pipe//docker_engine")
+	} else if _, exists := os.LookupEnv("DOCKER_HOST"); !exists {
 		os.Setenv("DOCKER_HOST", "unix://"+GetDockerSocket())
 	}
 
